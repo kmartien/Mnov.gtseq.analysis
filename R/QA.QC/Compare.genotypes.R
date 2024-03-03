219c55577a5bfbae31b1cbf83999874c6fde8f35
kmartien <115281269+kmartien@users.noreply.github.com>
2024-03-03 08:21:10 -0800

    Initial commit
    Starting QA/QC and analysis of completed GTseq data

---
+++ R/QA.QC/Compare.genotypes.R
@@ -0,0 +1,39 @@
+library(dplyr)
+library(tidyverse)
+library(swfscMisc)
+source("/Users/Shared/KKMDocuments/Documents/Github.Repos/Mnov/GTseq.design.and.QAQC/R/functions/tgt.2.geno.table.R")
+
+mismatched.samples <- c("z0196894.RunMS54", "z0107078.RunMS51", "z0197663.RunMS54", "z0196841.RunMS54", "z0053845.RunMS51")
+
+projects <- list("GTseq.prod", "GTseq.val", "RunMS43", "RunMS45.90s", "RunMS51", "RunMS54", "RunMS58")
+tgt.list <- lapply(projects, function(p){
+  load(paste0("results-R/", p, ".10readsMin.geno.eval.rda"))
+  tgt$Indiv <- paste0(tgt$Indiv, ".", p)
+  return(tgt)
+})
+names(tgt.list) <- projects
+
+tgt <- do.call(rbind, tgt.list)
+
+geno.table <- tgt.2.geno.table(tgt)
+
+genos.to.find <- geno.table[which(geno.table$Indiv %in% mismatched.samples),]
+
+geno.table <- geno.table[-which(geno.table$Indiv %in% mismatched.samples),]
+
+num.geno.matches <- function(genos.to.find, geno.table){
+  num.matches <- lapply(1:nrow(genos.to.find), function(x){
+    target.geno <- genos.to.find[x,-1]
+    diffs <- do.call(bind_rows, lapply(1:nrow(geno.table), function(y){
+      id <- geno.table[y,1]
+      gt <- geno.table[y,-1]
+      overlap <- sum(!is.na(gt[1,which(!is.na(target.geno))]))
+      matches <- sum(gt[1,] == target.geno, na.rm = TRUE)
+      return(c(id = id, overlap = overlap, matches = matches))
+    })) 
+  })
+  names(num.matches) <- genos.to.find$Indiv
+  return(num.matches)
+}
+
+save(num.matches, file = "results-R/Identifying.mixed.up.samples.rda")
